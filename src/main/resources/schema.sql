1b4185b9fee53e621449ce5146d8c458ecdbc005
zincbayz <zincbayz@yandex.ru>
2023-01-26 21:48:04 +0300

    Добавлена база данных постман тесты

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -51,3 +51,5 @@ CREATE TABLE IF NOT EXISTS Invites (
     status varchar
 );
 
+INSERT INTO Mpa (mpa_name) VALUES ('G'), ('PG'), ('PG-13'), ('R'), ('NC-17');
+INSERT INTO Genres (name) VALUES ('Комедия'), ('Драма'), ('Мультфильм'), ('Триллер'), ('Документальный'), ('Боевик');
