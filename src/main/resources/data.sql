a8a18c8c6706ef98a9d997bb662b510e082345a2
zincbayz <zincbayz@yandex.ru>
2023-01-26 23:25:52 +0300

    Добавлена база данных4

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
 INSERT INTO Mpa (mpa_name) VALUES ('G'), ('PG'), ('PG-13'), ('R'), ('NC-17');
 INSERT INTO Genres (name) VALUES ('Комедия'), ('Драма'), ('Мультфильм'), ('Триллер'), ('Документальный'), ('Боевик');
+
